c2952cc49928eee97379e35c035b1349a3600655
mayankdobhal0025 <mayankdobhal0025@gmail.com>
2026-04-24 08:53:27 +0000

    Pipeline

---
+++ pipeline/pipeline.py
@@ -0,0 +1,10 @@
+import sys
+print("arguments:", sys.argv)
+
+import pandas as pd
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
+print(f"Running pipeline for day {sys.argv[1]}")
