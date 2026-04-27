1b6d4733c1a0b01f4b99ffa6dddfaf6bc4e44784
mayankdobhal0025 <mayankdobhal0025@gmail.com>
2026-04-27 05:13:10 +0000

    added docker-compose file

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -1,51 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
 import pandas as pd
-
-
-# In[2]:
-
-
-pd.__file__
-
-
-# In[4]:
-
-
-prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow/'
-url = f'{prefix}/yellow_tripdata_2021-01.csv.gz'
-
-
-# In[5]:
-
-
-url
-
-
-# In[6]:
-
-
-df= pd.read_csv(url)
-
-
-# In[7]:
-
-
-df.head()
-
-
-# In[8]:
-
-
-df.columns
-
-
-# In[10]:
-
+from sqlalchemy import create_engine
+from tqdm.auto import tqdm
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -65,106 +24,77 @@ dtype = {
     "total_amount": "float64",
     "congestion_surcharge": "float64"
 }
+
 parse_dates = [
     "tpep_pickup_datetime",
     "tpep_dropoff_datetime"
 ]
 
-df = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates
-)
 
+def ingest_data(
+        url: str,
+        engine,
+        target_table: str,
+        chunksize: int = 100000,
+) -> pd.DataFrame:
+    df_iter = pd.read_csv(
+        url,
+        dtype=dtype,
+        parse_dates=parse_dates,
+        iterator=True,
+        chunksize=chunksize
+    )
 
-# In[11]:
+    first_chunk = next(df_iter)
 
+    first_chunk.head(0).to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="replace"
+    )
 
-len(df)
+    print(f"Table {target_table} created")
 
+    first_chunk.to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="append"
+    )
 
-# In[12]:
+    print(f"Inserted first chunk: {len(first_chunk)}")
 
+    for df_chunk in tqdm(df_iter):
+        df_chunk.to_sql(
+            name=target_table,
+            con=engine,
+            if_exists="append"
+        )
+        print(f"Inserted chunk: {len(df_chunk)}")
 
-df.head()
+    print(f'done ingesting to {target_table}')
+@click.command()
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pass', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', default='5432', help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--year', default=2021, type=int, help='Year of the data')
+@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 
+def main(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
+    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    url_prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
 
-# In[13]:
+    url = f'{url_prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz'
 
+    ingest_data(
+        url=url,
+        engine=engine,
+        target_table=target_table,
+        chunksize=chunksize
+    )
 
-df['tpep_pickup_datetime'].head()
-
-
-# In[14]:
-
-
-df.isnull().sum()
-
-
-# In[15]:
-
-
-get_ipython().system('uv add sqlalchemy')
-
-
-# In[16]:
-
-
-get_ipython().system('uv add psycopg2-binary')
-
-
-# In[18]:
-
-
-from sqlalchemy import create_engine
-engine = create_engine('postgresql://root:root@localhost:5432/ny_taxi')
-
-
-# In[19]:
-
-
-print(pd.io.sql.get_schema(df, name='yellow_taxi_data', con=engine))
-
-
-# In[29]:
-
-
-df.head(n=0).to_sql(name='yellow_taxi_data', con=engine, if_exists='replace')
-
-
-# In[20]:
-
-
-df_iter = pd.read_csv(
-    url,
-    dtype=dtype,
-    parse_dates=parse_dates,
-    iterator = True,
-    chunksize = 100000
-)
-
-
-# In[21]:
-
-
-get_ipython().system('uv add tqdm')
-
-
-# In[22]:
-
-
-from tqdm.auto import tqdm
-
-
-# In[25]:
-
-
-for df_chunk in tqdm(df_iter):
-    df_chunk.to_sql(name = 'yellow_taxi_data', con = engine, if_exists = 'append')
-
-
-# In[ ]:
-
-
-
-
+if __name__ == '__main__':
+    main()
